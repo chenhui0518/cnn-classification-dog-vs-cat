b5058367fd01b803597d37acb0a56264d48d0a68
zlxy9892 <zlxy9892@live.com>
2018-04-09 14:30:27 +0800

    update data_helper

--- data_helper.py
+++ data_helper.py
@@ -29,6 +29,11 @@ def img_resize(img_path, img_height, img_width):
     img_resized = cv2.resize(img_src, (img_height,img_width), interpolation=cv2.INTER_CUBIC)
     return img_resized
 
+def rgb2gray(img_rgb):
+    img_gray = np.dot(img_rgb[...,:3], [0.299, 0.587, 0.114])
+    img_gray = img_gray / 255.0
+    return img_gray.reshape(img_rgb.shape[0], img_rgb.shape[1], 1)
+
 def batch_iter(batch_size, num_epochs, img_path_list, label_list,
         img_height, img_width, shuffle=True):
     '''
@@ -52,22 +57,9 @@ def batch_iter(batch_size, num_epochs, img_path_list, label_list,
             img_list_shuffled = []
             for i in range(start_index, end_index):
                 img_data = img_resize(img_path=img_path_list_shuffled[i], img_height=img_height, img_width=img_width)
-                img_data_min, img_data_max = np.min(img_data), np.max(img_data)
-                img_data = (img_data - img_data_min) / (img_data_max - img_data_min)
+                # img_data_min, img_data_max = np.min(img_data), np.max(img_data)
+                # img_data = (img_data - img_data_min) / (img_data_max - img_data_min)
+                img_data = rgb2gray(img_data)
                 img_list_shuffled.append(img_data)
             img_list_shuffled = np.array(img_list_shuffled)
             yield img_list_shuffled, label_list_shuffled[start_index:end_index]
-
-    # img_path_list = tf.cast(img_path_list, tf.string)
-    # label_list = tf.cast(label_list, tf.int32)
-    # input_queue = tf.train.slice_input_producer([img_path_list, label_list])
-    # img_contents = tf.read_file(input_queue[0])
-    # imgs = tf.image.decode_jpeg(contents=img_contents, channels=channels)
-    # imgs = tf.image.resize_image_with_crop_or_pad(imgs, img_height, img_width)
-    # xs = tf.image.per_image_standardization(imgs)
-    # ys = input_queue[1]
-    # x_batch, y_batch = tf.train.batch([xs, ys], batch_size=batch_size, num_threads=n_threads, capacity=capacity)
-    # x_batch = tf.cast(x_batch, tf.float32)
-    # y_batch = tf.reshape(y_batch, [batch_size])
-    # y_batch = tf.cast(y_batch, tf.float32)
-    # return x_batch, y_batch
